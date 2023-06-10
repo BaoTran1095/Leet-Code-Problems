a9cdd3017c299cc348bebc82798c4b33797d7b65
BaoTran1095 <109429977+BaoTran1095@users.noreply.github.com>
2023-06-10 04:05:04 -0700

    Add files via upload

---
+++ MergeLinkedList.java
@@ -0,0 +1,63 @@
+
+public class MergeLinkedList {
+	public static void main(String[] args) {
+		ListNode list1 = new ListNode(1);
+		list1.next = new ListNode(2);
+		list1.next.next = new ListNode(4);
+		
+		ListNode list2 = new ListNode(1);
+		list2.next = new ListNode(3);
+		list2.next.next = new ListNode(4);
+		
+		mergeTwoLists(list1, list2);
+	}
+	
+	
+	public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		ListNode ans = new ListNode();
+		ListNode temp = ans;
+		if(list1 == null && list2 == null){return null;}
+        if(list1 == null && list2 != null){return list2;}
+        if(list1 != null && list2 == null){return list1;}
+		
+		while(list1.next != null || list2.next!= null) {
+			if(list1.val == list2.val) {
+				temp.next = new ListNode(list1.val);
+				temp = temp.next;
+				temp.next = new ListNode(list2.val);
+				temp = temp.next;
+				
+				list1 = list1.next;
+				list2 = list2.next;
+			}if(list1.val < list2.val) {
+				temp.next = new ListNode(list1.val);
+				temp = temp.next;
+				list1 = list1.next;
+			}else {
+				temp.next = new ListNode(list2.val);
+				temp = temp.next;
+				list2 = list2.next;
+			}
+		}
+		
+		if(list1 != null) {
+			temp.next = list1;
+		}else {
+			temp.next = list2;
+		}
+		
+		return ans.next;
+
+	}
+
+	
+	
+	public static class ListNode {
+		int val;
+		ListNode next;
+		ListNode() {}
+		ListNode(int val) { this.val = val; }
+		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+		public String toString(){ return ""; }
+	}
+}
