5fbcd8cebaec6c4be8d94f58f84f35e769b70752
BaoTran1095 <109429977+BaoTran1095@users.noreply.github.com>
2023-07-06 04:17:12 -0700

    Add files via upload

---
+++ Number_of_Equivalent_Domino_Pairs.java
@@ -0,0 +1,29 @@
+
+public class Number_of_Equivalent_Domino_Pairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [][] pairs = {{1,2}
+						 ,{2,1}
+						 ,{3,4}
+						 ,{5,6}};
+		int [][] pairs2 = {};
+		System.out.println(pairs2.length);
+		System.out.println(numEquivDominoPairs(pairs2));
+	}
+	
+	public static int numEquivDominoPairs(int[][] dominoes) {
+		int count = 0;
+		if (dominoes.length == 1 ||dominoes.length == 0) {return count;}
+        for (int i = 0; i < dominoes.length; i++) {
+        	for (int j = i+1; j < dominoes.length; j++) {
+            	if(((dominoes[i][0] == dominoes[j][0]) && (dominoes[i][1] == dominoes[j][1]))
+            	|| ((dominoes[i][0] == dominoes[j][1]) && (dominoes[i][1] == dominoes[j][0]))) {
+            		count++;
+            	}
+            }
+        	
+        }
+		return count;
+    }
+}
